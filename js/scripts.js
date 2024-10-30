d9ed9de7942ac965826170b6537d6cec884b2ddc
FOS <panelfluxv@gmail.com>
2024-10-30 20:11:13 +0530

    Delete js directory

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-// Toggle between login and register forms
-document.getElementById('toggle-to-login').addEventListener('click', function() {
-    document.getElementById('login-form').style.display = 'block';
-    document.getElementById('register-form').style.display = 'none';
-});
-
-// You can add more JavaScript functions here for handling form submissions, validation, etc.
